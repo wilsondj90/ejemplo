835e0986bed934376f45ba3109f0f86e8b5a6737
Wilson De Jesús Correa Berrio <Wcorrea@intergrupo.com>
2019-01-21 15:14:19 -0500

    se cambia world por sister

---
+++ ejemplo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello sister!");
+            
+        }
+    }
+}
